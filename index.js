cb03271ab48bd2c9a0a55c89b1b6e51d49a5e272
saifularafat <saifularafat.info@gmail.com>
2023-05-02 23:54:33 +0600

    master chef json added

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const master_chef = require ('./data/chef.json')
+
 app.get('/', (req, res) => {
     res.send('Hi Chef Server')
 })
+
+app.get('/chef', (req, res) => {
+    res.send(master_chef)
+})
+
 
 app.listen(port, () => {
     console.log(`Server site Explore by :${port}`);
