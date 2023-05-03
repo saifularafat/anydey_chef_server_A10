891a55606ea0ca888173232b1f062f77bef7b3d9
saifularafat <saifularafat.info@gmail.com>
2023-05-03 21:01:26 +0600

    the food daynamic id complate

--- index.js
+++ index.js
@@ -13,16 +13,29 @@ app.get('/', (req, res) => {
     res.send('Hi Chef Server')
 })
 
+// chef local server deploy
 app.get('/chef', (req, res) => {
     res.send(master_chef)
 })
 
+// chef dynamic id
 app.get('/chef/:id', (req, res) => {
     const id = req.params.id;
     const selectChef = master_chef.find(chef => chef._id == id);
     res.send(selectChef);
 })
 
+// food local server deploy
+app.get('/food', (req, res) => {
+    res.send(chef_food)
+})
+
+//food dynamic id
+app.get('/food/:id', (req, res) => {
+    const id = req.params.id;
+    const selectFood = chef_food.find(food => food.id == id);
+    res.send(selectFood);
+})
 app.listen(port, () => {
     console.log(`Server site Explore by :${port}`);
 })
