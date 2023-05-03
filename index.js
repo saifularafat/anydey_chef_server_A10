b7da8541799e41957ce74a05d3d94ddf0679e963
saifularafat <saifularafat.info@gmail.com>
2023-05-03 17:25:52 +0600

    local server theka dynamic id added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
+
+app.use(cors())
 
 const master_chef = require ('./data/chef.json')
 
@@ -12,6 +15,11 @@ app.get('/chef', (req, res) => {
     res.send(master_chef)
 })
 
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectChef = master_chef.find(chef => chef._id == id);
+    res.send(selectChef);
+})
 
 app.listen(port, () => {
     console.log(`Server site Explore by :${port}`);
