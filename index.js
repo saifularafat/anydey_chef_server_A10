5f328b44a87f1116fc7358ed213865f99c5a5151
saifularafat <saifularafat.info@gmail.com>
2023-05-03 21:17:36 +0600

    Chef and foodadd two in one dynamic api create

--- index.js
+++ index.js
@@ -13,28 +13,35 @@ app.get('/', (req, res) => {
     res.send('Hi Chef Server')
 })
 
-// chef local server deploy
+// chef local server api create
 app.get('/chef', (req, res) => {
     res.send(master_chef)
 })
 
 // chef dynamic id
-app.get('/chef/:id', (req, res) => {
-    const id = req.params.id;
-    const selectChef = master_chef.find(chef => chef._id == id);
-    res.send(selectChef);
-})
+// app.get('/chef/:id', (req, res) => {
+//     const id = req.params.id;
+//     const selectChef = master_chef.find(chef => chef._id == id);
+//     res.send(selectChef);
+// })
 
-// food local server deploy
+// food local server api create
 app.get('/food', (req, res) => {
     res.send(chef_food)
 })
 
-//food dynamic id
+//food dynamic id api create
 app.get('/food/:id', (req, res) => {
     const id = req.params.id;
     const selectFood = chef_food.find(food => food.id == id);
     res.send(selectFood);
+})
+
+// master_chef and chef_food added and the dynamic api id create
+app.get('/chef/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const masterId = chef_food.filter(food =>parseInt(food.chef_id) === id);
+    res.send(masterId)
 })
 app.listen(port, () => {
     console.log(`Server site Explore by :${port}`);
